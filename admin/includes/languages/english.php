20539c8f261dacc1244ee9808686e4eca9a5c1f9
“Alaa4eg-h” <Alaa4eg@outlook.com>
2021-11-21 16:16:35 +0200

    Ecommerce Site Base Structure

---
+++ admin/includes/languages/english.php
@@ -0,0 +1,18 @@
+<?php
+
+    function lang($phrase) {
+        static $lang = array(
+            // HOME PAGE
+            
+            'MESSAGE' =>  'Welcome',
+            'ADMIN'   =>  'Administrator'
+
+        );
+        return $lang[$phrase];
+    }
+
+
+
+
+
+?>
---
+++ admin/init.php
@@ -0,0 +1,11 @@
+
+
+<?php
+
+
+// Routes
+
+
+$tpl = 'includes/templates/';  // Template Directory
+$css = 'layout/css/'; // CSS Directory
+$js = 'layout/js/';  // JS Directory
